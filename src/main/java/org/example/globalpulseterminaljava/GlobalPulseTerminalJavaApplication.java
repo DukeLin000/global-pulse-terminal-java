3afdc731dee79f63ee687d1e458cc66709a30894
DukeLin000 <thomas831215007@gmail.com>
2026-04-24 15:36:47 +0800

    FIRST

---
+++ src/main/java/org/example/globalpulseterminaljava/GlobalPulseTerminalJavaApplication.java
@@ -0,0 +1,13 @@
+package org.example.globalpulseterminaljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalPulseTerminalJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalPulseTerminalJavaApplication.class, args);
+    }
+
+}
